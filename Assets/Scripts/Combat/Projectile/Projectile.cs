e0fccd5933584cdc77283d743b5286bb08ed2658
Baihua <51917555+silkmoon18@users.noreply.github.com>
2023-09-23 00:04:27 -0700

    Set up

--- Assets/Scripts/Combat/Projectile/Projectile.cs
+++ Assets/Scripts/Combat/Projectile/Projectile.cs
@@ -10,45 +10,34 @@ namespace CSCI526GameJam {
         #region Fields
         [ClassHeader(typeof(Projectile))]
 
-        [MandatoryFields]
-        [SerializeField] protected float radius;
-
         [ComputedFields]
         [SerializeField] protected float damage;
-        [SerializeField] protected float impact;
-        [SerializeField] protected LayerMask targetLayerMask;
+        //[SerializeField] protected float impact;
         #endregion
 
         #region Publics
-        public float Radius => radius;
 
         /// <summary>
         /// Set up the basic properties. 
         /// </summary>
-        public virtual void Setup(Vector3 position, float damage, float impact, LayerMask targetLayerMask) {
+        public virtual void Setup(Vector3 position, float damage) {
             transform.position = position;
             this.damage = damage;
-            this.impact = impact;
-            this.targetLayerMask = targetLayerMask;
         }
 
         #endregion
 
         #region Internals
-        protected virtual void Dispose() {
-            OnDispose();
+        protected abstract void OnDisposed();
+
+        protected void Dispose() {
+            OnDisposed();
             ProjectilePooler.Instance.Release(this);
         }
 
-        protected virtual void OnDispose() {
-            //damage = 0f;
-        }
         #endregion
 
         #region Unity Methods
-        protected virtual void OnDrawGizmos() {
-            Gizmos.DrawWireSphere(transform.position, radius);
-        }
         #endregion
     }
 }
