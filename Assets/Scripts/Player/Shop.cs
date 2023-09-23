1b07a78d77e0a68e62ae45a16ddf133e7780b82b
Baihua <51917555+silkmoon18@users.noreply.github.com>
2023-09-22 20:31:58 -0700

    Squashed commit of the following:
    commit a64069a1cbbd9f0ce00048cde04dc70e432f33a6
    Author: Baihua <51917555+silkmoon18@users.noreply.github.com>
    Date:   Fri Sep 22 20:21:00 2023 -0700
    
        Squashed commit of the following:
    
        commit 0fc70b427adf71c6f82a6e1c9278f7340e358785
        Author: Baihua <51917555+silkmoon18@users.noreply.github.com>
        Date:   Fri Sep 22 20:20:44 2023 -0700
    
            Update .gitignore
    
        commit 6d3cf29e79fad39212f9edec17371ab5169cccf5
        Author: Baihua <51917555+silkmoon18@users.noreply.github.com>
        Date:   Fri Sep 22 20:18:05 2023 -0700
    
            Complete merging from UI
    
        commit d3fb9c6dd07227da94c3662787ad3f023350fda4
        Merge: 3f4fe61 dabacf6
        Author: Baihua <51917555+silkmoon18@users.noreply.github.com>
        Date:   Fri Sep 22 19:25:55 2023 -0700
    
            Merge branch 'UI' into feature/shop-system
    
        commit dabacf63095e2439c2feff5cd8b620096167a6cc
        Author: Baihua <51917555+silkmoon18@users.noreply.github.com>
        Date:   Fri Sep 22 19:25:14 2023 -0700
    
            Add prefix UI to UI classes
    
        commit 7bc99e57f3f06bbcd3bfa5f235c3ad88a18963f2
        Author: zmisrio <mzhang12@usc.edu>
        Date:   Fri Sep 22 18:41:09 2023 -0700
    
            first version of UI
    
        commit 3f4fe61ace8f4e74a4c29c4abfd3c7f7d67ab2c7
        Author: Baihua <51917555+silkmoon18@users.noreply.github.com>
        Date:   Fri Sep 22 17:33:46 2023 -0700
    
            add shop

---
+++ Assets/Scripts/Player/Shop.cs
@@ -0,0 +1,117 @@
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CSCI526GameJam {
+    public class Shop : MonoBehaviourSingleton<Shop> {
+
+        #region Fields
+        [ComputedFields]
+        [SerializeField] private int gold;
+        [ShowInInspector] private Dictionary<ItemConfig, Item> configToItem = new();
+        [ShowInInspector] private Dictionary<ItemRank, List<Item>> rankToItem = new();
+
+        [SerializeField] private List<ItemConfig> itemConfigs;
+#if UNITY_EDITOR
+        [EditorOnlyFields]
+        [FolderPath, SerializeField]
+        private string itemConfigsPath;
+
+        [Button("Find Item Configs", ButtonSizes.Large)]
+        private void FindAssets() {
+            itemConfigs = Utility.FindRefsInFolder<ItemConfig>(itemConfigsPath);
+            Debug.Log($"Found {itemConfigs.Count} item configs under {itemConfigsPath}. ");
+        }
+#endif
+        #endregion
+
+        #region Publics
+        public event Action<Item> OnItemAdded;
+
+        public int Gold => gold;
+
+        /// <summary>
+        /// Get a random item from given rank. 
+        /// </summary>
+        /// <param name="rank">Rank of the item. </param>
+        /// <returns>A random item. </returns>
+        public ItemConfig GetRandom(ItemRank rank) {
+            var items = rankToItem[rank];
+            var index = Random.Range(0, items.Count);
+            return items[index].Config;
+        }
+
+        /// <summary>
+        /// Try to buy a item. 
+        /// </summary>
+        /// <param name="config">Config of the item. </param>
+        public void Buy(ItemConfig config) {
+            if (config.Price > gold) return;
+
+            var item = configToItem[config];
+            if (item.NumAvailable == 0) {
+                Debug.LogWarning($"Trying to buy an item {config.ItemName} with 0 numAvailable. ");
+                return;
+            }
+
+            gold -= config.Price;
+            item.Add();
+            OnItemAdded?.Invoke(item);
+        }
+
+        // TODO: To be modified. 
+        /// <summary>
+        /// Try to sell an item. 
+        /// </summary>
+        /// <param name="config">Config of the item. </param>
+        public void Sell(ItemConfig config) {
+            var item = configToItem[config];
+            if (item.NumStacked == 0) {
+                Debug.LogWarning($"Trying to sell an item {config.ItemName} with 0 numStacked. ");
+                return;
+            }
+
+            item.Remove();
+            gold += config.Price;
+        }
+
+        /// <summary>
+        /// (Debug) Add all items, ignoring numAvailable and price. 
+        /// </summary>
+        /// <param name="num">Num of each item to add. </param>
+        public void Debug_addItems(int num = 1) {
+            foreach (var config in itemConfigs) {
+                for (int i = 0; i < num; i++) {
+                    var item = configToItem[config];
+                    item.Add();
+                    OnItemAdded?.Invoke(item);
+                }
+            }
+        }
+        #endregion
+
+        #region Internals
+        #endregion
+
+        #region Unity Methods
+        protected override void Awake() {
+            base.Awake();
+
+            // Init items by its config and rank. 
+            foreach (var config in itemConfigs) {
+                var item = new Item(config);
+                configToItem.Add(config, item);
+
+                var rank = config.Rank;
+                if (!rankToItem.ContainsKey(rank)) {
+                    rankToItem[rank] = new();
+                }
+                rankToItem[rank].Add(item);
+            }
+        }
+        #endregion
+    }
+}
