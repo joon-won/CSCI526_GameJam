99b43289613be5fdc666fde7cd6f7844b3cf28f0
zmisrio <mzhang12@usc.edu>
2023-09-25 23:37:04 -0400

    Shop display and golds display

--- Assets/Scripts/UI/UICombat.cs
+++ Assets/Scripts/UI/UICombat.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +10,8 @@ namespace CSCI526GameJam
     {
         public GameObject CombatPanel;
         public GameObject ControlPanel;
-
+        public TextMeshProUGUI goldsInfo;
+        public TextMeshProUGUI baseInfo;
         public Button StartCombat;
         // Start is called before the first frame update
         void Start()
@@ -17,7 +19,23 @@ namespace CSCI526GameJam
             StartCombat.onClick.AddListener(CombatStart);
             CombatPanel.SetActive(false);
         }
+        
+        void Update()
+        {
+            UpdateGoldsInfo();
+            // UpdateBaseInfo();
+        }
+        
+        private void UpdateGoldsInfo()
+        {
+            goldsInfo.text = "Golds: " + Player.Instance.Gold.ToString();
+        }
 
+        // private void UpdateBaseInfo()
+        // {
+        //     baseInfo.text = "BaseHP: " + TowerManager.Instance.PlayerBase.ToString();
+        // }
+        
         private void CombatStart()
         {
             CombatPanel.SetActive(true);
