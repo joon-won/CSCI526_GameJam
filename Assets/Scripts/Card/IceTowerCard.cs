01d71b2e23115a25906351640851d64751f65c4a
Ptqxpcs <ziran@ezze.live>
2023-10-16 20:07:50 -0700

    add tower cards

---
+++ Assets/Scripts/Card/IceTowerCard.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CSCI526GameJam
+{
+    public class IceTowerCard : Card
+    {
+        #region Fields
+        [ClassHeader(typeof(IceTowerCard))]
+        [SerializeField] private TowerConfig config;
+
+        private Player player = FindObjectOfType<Player>();
+        #endregion
+
+        #region Publics
+        public override void PlayLv1()
+        {
+            player.AddTower(config, 1);
+        }
+
+        public override void PlayLv2()
+        {
+            player.AddTower(config, 3);
+        }
+
+        public override void PlayLv3()
+        {
+            player.AddTower(config, 6);
+        }
+        #endregion
+
+        #region Internals
+        #endregion
+
+        #region Unity Methods
+        #endregion
+    }
+}
