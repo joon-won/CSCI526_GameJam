84b59a2966cd510add0013229b8dccd3556d6c34
jaydenli29 <jaydenli429@gmail.com>
2023-11-19 15:23:49 -0800

    Add a new scene for the help icon button, removed the previous pop up window

--- Assets/Scripts/UI/GoHelpPage.cs
+++ Assets/Scripts/UI/GoHelpPage.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 namespace CSCI526GameJam
 {
-    public class GamePlay : MonoBehaviour
-    {   
-        public void GameStart()
+    public class GoHelpPage : MonoBehaviour
+    {
+        public void HelpPage()
         {
-            SceneManager.LoadScene("GamePlay");
+            SceneManager.LoadScene("HelpPanel");
         }
     }
 }
