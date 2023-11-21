c7a85cbe617d813a8f3f712108a3f580a151ad08
zranfei <feizr424@gmail.com>
2023-11-21 01:33:09 -0800

    fix small issue

--- Assets/Scripts/Card/IceTowerCard.cs
+++ Assets/Scripts/Card/IceTowerCard.cs
@@ -10,27 +10,29 @@ namespace CSCI526GameJam
         #region Fields
         [ClassHeader(typeof(IceTowerCard))]
         [SerializeField] private TowerConfig config;
-        [SerializeField] private int numLv1;
-        [SerializeField] private int numLv2;
         [SerializeField] private int numLv3;
+        [SerializeField] private int numLv2;
+        [SerializeField] private int numLv1;
+
 
         #endregion
 
         #region Publics
-        public override void PlayLv1()
+        public override void PlayLv3()
         {
-            Player.Instance.AddTower(config, numLv1);
+            Player.Instance.AddTower(config, numLv3);
         }
-
+        
         public override void PlayLv2()
         {
             Player.Instance.AddTower(config, numLv2);
         }
 
-        public override void PlayLv3()
+        public override void PlayLv1()
         {
-            Player.Instance.AddTower(config, numLv3);
+            Player.Instance.AddTower(config, numLv1);
         }
+
         #endregion
 
         #region Internals
