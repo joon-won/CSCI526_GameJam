0ab96259a8eef63bd74dc0403ad05bd25cd2ea1c
zmisrio <mzhang12@usc.edu>
2023-11-21 16:39:36 -0800

    fix game play bug

--- Assets/Scripts/UI/HelpPanel.cs
+++ Assets/Scripts/UI/HelpPanel.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 namespace CSCI526GameJam
 {
-    public class GamePlay : MonoBehaviour
-    {   
+    public class HelpPanel : MonoBehaviour
+    {
         public void GameStart()
         {
-            SceneManager.LoadScene("GamePlay");
+            SceneManager.UnloadSceneAsync("HelpPanel");
         }
     }
 }
